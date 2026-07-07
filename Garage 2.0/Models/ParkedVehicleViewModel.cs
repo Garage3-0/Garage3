1d9830591a4ce1a2ecb46cb0f2f4ac12f85110a1
ChristianLindqvist <christian.g.lindqvist@gmail.com>
2026-07-07 14:00:37 +0200

    Added error messages for validation errors.

--- Garage 2.0/Models/ParkedVehicleViewModel.cs
+++ Garage 2.0/Models/ParkedVehicleViewModel.cs
@@ -4,25 +4,33 @@ namespace Garage_2._0.Models
 {
     public class ParkedVehicleViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Please select a vehicle type.")]
+        [Display(Name = "Vehicle type")]
         public VehicleType VehicleType { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Registration number is required.")]
+        [StringLength(10, ErrorMessage = "Registration number is too long.")]
+        [Display(Name = "Registration number")]
         public string RegNbr { get; set; }
 
-        [Required]
-        [StringLength(10)]
+        [Required(ErrorMessage = "Color is required.")]
+        [StringLength(10, ErrorMessage = "Color name is too long.")]
+        [Display(Name = "Color")]
         public string Color { get; set; }
 
-        [Required]
-        [StringLength(20)]
+        [Required(ErrorMessage = "Brand is required.")]
+        [StringLength(20, ErrorMessage = "Brand name is too long.")]
+        [Display(Name = "Brand")]
         public string Brand { get; set; }
 
-        [Required]
-        [StringLength(10)]
+        [Required(ErrorMessage = "Model is required.")]
+        [StringLength(10, ErrorMessage = "Model name is too long.")]
+        [Display(Name = "Model")]
         public string Model { get; set; }
 
-        [Range(2, 10)]
+        [Required(ErrorMessage = "Number of wheels is required.")]
+        [Range(2, 10, ErrorMessage = "Number of wheels must be between 2 and 10.")]
+        [Display(Name = "Number of wheels")]
         public int Wheels { get; set; }
     }
 }
