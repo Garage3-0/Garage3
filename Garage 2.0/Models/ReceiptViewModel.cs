1cfd3755f3a6aa5e86817ec3a03abb9fb49f268f
Göran Olson <goran@famolson.se>
2026-07-08 09:46:28 +0200

    Checkout and Receipt finished

---
+++ Garage 2.0/Models/ReceiptViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Garage_2._0.Models
+{
+    public class ReceiptViewModel
+    {
+        public required int Id { get; set; }
+
+        public VehicleType? VehicleType { get; set; }
+        
+        public required string RegNbr { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Model { get; set; }
+
+        public int? Wheels { get; set; }
+
+        public required DateTime Arrival { get; set; }
+
+        public required DateTime CheckoutTime { get; set; }
+
+        public required int ParkedDays { get; set; }
+
+        public required int ParkedHours { get; set; }
+
+        public required int ParkedMinutes { get; set; }
+
+        public required int Price { get; set; }
+
+        public required int PricePerHour { get; set; }
+    }
+}
