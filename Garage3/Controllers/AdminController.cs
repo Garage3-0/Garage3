9472c9e43b1e5d7bbd7968fb2a5e8367de452d47
Christian Lindqvist <christian.g.lindqvist@gmail.com>
2026-08-12 13:55:57 +0200

    Add admin user listing and details views

---
+++ Garage3/Controllers/AdminController.cs
@@ -0,0 +1,73 @@
+﻿
+
+using Garage3.Constants;
+using Garage3.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage3.Controllers;
+
+[Authorize(Roles = Roles.Admin)]
+public class AdminController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AdminController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Users()
+    {
+        var users = await _userManager.Users
+            .OrderBy(u => u.UserName)
+            .ToListAsync();
+
+        var usersWithRoles = new List<(ApplicationUser User, IList<string> Roles)>();
+
+        foreach (var user in users)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            usersWithRoles.Add((user, roles));
+        }
+
+        return View(usersWithRoles);
+    }
+
+    public async Task<IActionResult> UserDetails(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            return NotFound();
+        }
+
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        return View((user, roles));
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
